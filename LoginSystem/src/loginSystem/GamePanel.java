72e4e9e36662a325f4fc5ee90a56fdaf075ab9d4
ttomczak <ttomczak1512@protonmail.com>
2021-07-05 23:59:50 -0500

    Finished setting up GamePanel

--- LoginSystem/src/loginSystem/GamePanel.java
+++ LoginSystem/src/loginSystem/GamePanel.java
@@ -1,9 +1,9 @@
 package loginSystem;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
 @SuppressWarnings("serial")
 public class GamePanel extends JPanel implements ActionListener {
@@ -13,9 +13,46 @@ public class GamePanel extends JPanel implements ActionListener {
 		
 	}
 	
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkApple() {
+		
+	}
+	
+	public void checkCollisions() {
+		
+	}
+	
+	public void gameOver(Graphics g) {
+		
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter {
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
 		
 	}
 
