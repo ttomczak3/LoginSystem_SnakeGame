1abdf609371cdce3845e064b78734777a99e967e
ttomczak <ttomczak1512@protonmail.com>
2021-07-08 18:35:19 -0500

    Score and game over

--- LoginSystem/src/loginSystem/GamePanel.java
+++ LoginSystem/src/loginSystem/GamePanel.java
@@ -59,30 +59,43 @@ public class GamePanel extends JPanel implements ActionListener {
 	
 	public void draw(Graphics g) {
 		
-		// Draws the grid
-		for (int i = 0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
+		if (running) {
+		
+			// Draws the grid
+			/*for (int i = 0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
+				
+				g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+				g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+				
+			}*/
 			
-			g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-			g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+			// Drawing the apple
+			g.setColor(Color.red);
+			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 			
+			// Drawing the snake
+			for (int i = 0; i < bodyParts; i++) {
+				
+				if (i == 0) {
+					g.setColor(Color.green);
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+				else {
+					g.setColor(new Color(45, 180, 0));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+				
+			}
+			
+			// Score text
+			g.setColor(Color.red);
+			g.setFont(new Font("Ink Free", Font.BOLD, 40));
+			FontMetrics metrics = getFontMetrics(g.getFont());
+			g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
+		
 		}
-		
-		// Drawing the apple
-		g.setColor(Color.red);
-		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-		
-		// Drawing the snake
-		for (int i = 0; i < bodyParts; i++) {
-			
-			if (i == 0) {
-				g.setColor(Color.green);
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-			}
-			else {
-				g.setColor(new Color(45, 180, 0));
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-			}
-			
+		else {
+			gameOver(g);
 		}
 		
 	}
@@ -125,6 +138,15 @@ public class GamePanel extends JPanel implements ActionListener {
 	
 	public void checkApple() {
 		
+		// Checking if the snake eats the apple
+		if ((x[0] == appleX) && (y[0] == appleY)) {
+			
+			bodyParts++;
+			applesEaten++; // The score
+			newApple();
+			
+		}
+		
 	}
 	
 	public void checkCollisions() {
@@ -166,6 +188,18 @@ public class GamePanel extends JPanel implements ActionListener {
 	
 	public void gameOver(Graphics g) {
 		
+		// Score text
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free", Font.BOLD, 40));
+		FontMetrics metrics1 = getFontMetrics(g.getFont());
+		g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
+		
+		// Game over text
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free", Font.BOLD, 75));
+		FontMetrics metrics2 = getFontMetrics(g.getFont());
+		g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over")) / 2, SCREEN_HEIGHT / 2);
+		
 	}
 	
 	@Override
@@ -189,31 +223,26 @@ public class GamePanel extends JPanel implements ActionListener {
 			
 			// Adding player controls
 			switch (e.getKeyCode()) {
-			
 			case KeyEvent.VK_A:
 				if (direction != 'R') {
 					direction = 'L';
 				}
 				break;
-				
 			case KeyEvent.VK_D:
 				if (direction != 'L') {
 					direction = 'R';
 				}
 				break;
-				
 			case KeyEvent.VK_W:
 				if (direction != 'D') {
 					direction = 'U';
 				}
 				break;
-				
 			case KeyEvent.VK_S:
 				if (direction != 'U') {
 					direction = 'D';
 				}
 				break;
-			
 			}
 			
 		}
