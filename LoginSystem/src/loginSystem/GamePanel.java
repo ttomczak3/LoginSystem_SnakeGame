e67b3a31828641c13e523355561c23496c1b86c7
ttomczak <ttomczak1512@protonmail.com>
2021-07-07 22:07:34 -0500

    Added an apple

--- LoginSystem/src/loginSystem/GamePanel.java
+++ LoginSystem/src/loginSystem/GamePanel.java
@@ -39,6 +39,7 @@ public class GamePanel extends JPanel implements ActionListener {
 		
 	}
 	
+	// Starting everything needed at the beginning of the game
 	public void startGame() {
 		
 		newApple();
@@ -48,6 +49,7 @@ public class GamePanel extends JPanel implements ActionListener {
 		
 	}
 	
+	// Displaying all of the graphics
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
@@ -57,6 +59,7 @@ public class GamePanel extends JPanel implements ActionListener {
 	
 	public void draw(Graphics g) {
 		
+		// Draws the grid
 		for (int i = 0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
 			
 			g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
@@ -64,9 +67,17 @@ public class GamePanel extends JPanel implements ActionListener {
 			
 		}
 		
+		// Drawing the apple
+		g.setColor(Color.red);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+		
 	}
 	
+	// Spawning the apple on a random coordinate
 	public void newApple() {
+		
+		appleX = random.nextInt((int)SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
+		appleY = random.nextInt((int)SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
 		
 	}
 	
